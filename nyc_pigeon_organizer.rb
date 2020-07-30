1caa4975301833f702d335dd0977a84978f9cd51
Anney Park <anneypark@gmail.com>
2020-07-30 04:19:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
+  
+  final_data = data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+        if !new_array[name]
+          new_array[name] = { }
         end 
         if !new_array[name][key]
-          !new_array[name][key] = {}
+          !new_array[name][key] = { }
         end
         new_array[name][key].push(inner_key.to_s)
       end
     end
   end
+  #binding.pry
 end
