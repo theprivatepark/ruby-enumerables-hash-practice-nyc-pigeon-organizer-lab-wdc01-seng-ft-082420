b3f787491fdc3f08d7df055dfe29c357fe41bc18
Anney Park <anneypark@gmail.com>
2020-07-30 04:18:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
-  final_data = data.each_with_object({}) do |(key, value), new_array|
+  final_results = data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
       names.each do |name|
         if !new_array[name]
