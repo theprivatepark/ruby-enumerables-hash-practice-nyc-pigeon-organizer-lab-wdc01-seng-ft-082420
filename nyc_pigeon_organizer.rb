f6d2f2117a21050d51b609f84c54e65e9a392e83
Anney Park <anneypark@gmail.com>
2020-07-30 04:22:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_array|
     binding.pry
+    value.each do |inner_key, names|
+      binding.pry
   end
 end
