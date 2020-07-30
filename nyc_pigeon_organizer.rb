a845e49e29809419cb608ecf55e11d59c7d6c1c1
Anney Park <anneypark@gmail.com>
2020-07-30 04:25:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
           new_array[name] = {}
         end 
         if !new_array[name][key]
-          !new_array[name][key] = {}
+          !new_array[name][key] = []
         end
         new_array[name][key].push (inner_key.to_s)
       end
